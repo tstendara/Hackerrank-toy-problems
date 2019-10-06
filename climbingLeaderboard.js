674960438c5dd8508e8f2f4efff0d2056554bfcd
Tyler Stendara <tstendara@gmail.com>
2019-10-05 21:04:40 -0500

    reset curHighestScore every player score

--- climbingLeaderboard.js
+++ climbingLeaderboard.js
@@ -9,30 +9,33 @@ const leaderBoard = (allScores, player) => {
     let curHighestScore = Infinity;
     let curNumber;
     let ranks = [];
-    let rank;;
+    let rank;
 
     for(let j=0; j<player.length; j++){
         rank = 0
+        curHighestScore = Infinity;
+
         for(let i=0; i<allScores.length; i++){
             curNumber = allScores[i]
-        
+            
             if(curNumber !== curHighestScore){
                 rank ++
-                curHighestScore = allScores[i]
+                curHighestScore = allScores[i] //rank: 1, curhighest = 100
             }
 
            if(player[j] > curNumber || player[j] === curNumber){ //handles when players score is greater than another score or if the score is the same in the leaderboard
                 ranks.push(rank)
                 break;
            }else if(i === allScores.length - 1){ //only if its last place
-                ranks.push(rank + 1)
+                ranks.push(rank + 1) 
                 break;
            }
         }
     }
     return ranks
 }   
-const res = leaderBoard([100, 100, 50, 40, 40, 20, 10], [5, 25, 50, 120])
+const res = leaderBoard([100, 100, 50, 40, 40, 20, 10], [120, 5, 25, 50, 5])
 console.log(res)
 
-//solves 6/12 cases currently
+
+
