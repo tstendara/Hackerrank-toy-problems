e69af613b09e9d0cb6012725b1d88b12433374da
Tyler Stendara <tstendara@gmail.com>
2019-10-10 16:07:20 -0500

    modified Two Sum with abstract data structure type Map

--- Two Sum.js
+++ Two Sum.js
@@ -3,20 +3,19 @@
 // Because nums[0] + nums[1] = 2 + 7 = 9,
 // return [0, 1].
 
-
 const TwoSum = (arr, target) => {
     const solutions = []
-    const hashTable = {}
+    const hashTable = new Map()
 
     for(let i = 0; i<arr.length; i++){
         
         let sumMinusElement = target - arr[i]
 
-        if(hashTable[sumMinusElement.toString()] !== undefined){
-            solutions.push([hashTable[sumMinusElement.toString()], i])
+        if(hashTable.has(sumMinusElement)){
+            solutions.push([hashTable.get(sumMinusElement), i])
         }
 
-        hashTable[arr[i].toString()] = i
+        hashTable.set(arr[i], i)
 
     }
     return solutions
@@ -24,3 +23,27 @@ const TwoSum = (arr, target) => {
 const res = TwoSum([2, 7, 11, 15], 9)
 console.log(res)
 
+
+
+
+
+
+// const TwoSum = (arr, target) => {
+//     const solutions = []
+//     const hashTable = {}
+
+//     for(let i = 0; i<arr.length; i++){
+        
+//         let sumMinusElement = target - arr[i]
+
+//         if(hashTable[sumMinusElement.toString()] !== undefined){
+//             solutions.push([hashTable[sumMinusElement.toString()], i])
+//         }
+
+//         hashTable[arr[i].toString()] = i
+
+//     }
+//     return solutions
+// }
+// const res = TwoSum([2, 7, 11, 15], 9)
+// console.log(res)
